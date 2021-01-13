d05f3374fcfe49cbc762666a91cb59f3d96d3f88
Fouad Chmainy <fchmainy@yahoo.com>
2021-01-13 23:28:38 +0100

    put some order

--- apigw/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t registry.gitlab.com/k8sfch/demo-name-generator/apigw --build-arg CONTROLLER_URL=https://controller.f5demolab.org:8443/1.4 --build-arg API_KEY='3bbb7951e9baebc92c69e37e083e49c9' --build-arg STORE_UUID=True --build-arg LOCATION=aks_apigw-ns .
-docker push registry.gitlab.com/k8sfch/demo-name-generator/apigw
-
--- apigw2/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t registry.gitlab.com/k8sfch/demo-name-generator/apigw2 --build-arg CONTROLLER_URL=https://controller.f5demolab.org:8443/1.4 --build-arg API_KEY='25b06e4fc0e7d029badbad4708621231' --build-arg STORE_UUID=True --build-arg LOCATION=aks .
-docker push registry.gitlab.com/k8sfch/demo-name-generator/apigw2
-
