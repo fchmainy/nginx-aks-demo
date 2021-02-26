e6529d76f253d1f2a41a0a3a908b4da371226adc
Fouad Chmainy <fchmainy@yahoo.com>
2021-02-26 19:51:44 +0100

    redirect apigw

--- Docker/apigw/README.md
+++
@@ -1,1 +0,0 @@
-please use **https://github.com/fchmainy/nginx_k8s_apigw**
--- Docker/apigw/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build -t registry.gitlab.com/--path--/apigw:v1.0.0 --build-arg CONTROLLER_URL=https://controller.myfqdn.com:8443/1.4 --build-arg API_KEY='APIKEYAPIKEYAPIKEYAPIKEYAPIKEYAPIKEYAPIKEY' --build-arg STORE_UUID=True --build-arg LOCATION=aks_apigw-ns .
-docker push registry.gitlab.com/k8sfch/demo-name-generator/apigw:v1.0.0
-
