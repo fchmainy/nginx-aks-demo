07b0b50068cf6b57d8c1ad609b81e611db0689c1
Fouad Chmainy <fchmainy@yahoo.com>
2021-02-26 19:48:05 +0100

    cleaning up

--- Docker/apigw/build.sh
+++ Docker/apigw/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t registry.gitlab.com/k8sfch/demo-name-generator/apigw --build-arg CONTROLLER_URL=https://controller.f5demolab.org:8443/1.4 --build-arg API_KEY='3bbb7951e9baebc92c69e37e083e49c9' --build-arg STORE_UUID=True --build-arg LOCATION=aks_apigw-ns .
-docker push registry.gitlab.com/k8sfch/demo-name-generator/apigw
+docker build -t registry.gitlab.com/--path--/apigw:v1.0.0 --build-arg CONTROLLER_URL=https://controller.myfqdn.com:8443/1.4 --build-arg API_KEY='APIKEYAPIKEYAPIKEYAPIKEYAPIKEYAPIKEYAPIKEY' --build-arg STORE_UUID=True --build-arg LOCATION=aks_apigw-ns .
+docker push registry.gitlab.com/k8sfch/demo-name-generator/apigw:v1.0.0
 
